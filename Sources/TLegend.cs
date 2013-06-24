c79d8ef69e87654c067fb1ebad2b0a5f49a8d046
Jan G. Wesseling <jan.wesseling@wur.nl>
2013-06-24 15:10:21 +0200

    First release

---
+++ Sources/TLegend.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace TDRFree
+{
+  class TLegend
+  {
+    private TDrawing Drawing;
+    private Int32 PlotWidth;
+    private Int32 PlotHeight;
+    private Color BackgroundColor;
+    private Color ForegroundColor;
+
+    //
+    //**********************************************************************************************
+    //
+    // Class TLegend
+    //   Part of  : TDRFree
+    //   Function : Contains the legend of the contourplots
+    //   Author   : Jan G. Wesseling
+    //   Date     : April 9th, 2013
+    //
+    //**********************************************************************************************
+    //
+    
+    public TLegend(Color aBackColor, Color aForeColor)
+    {
+      BackgroundColor = aBackColor;
+      ForegroundColor = aForeColor;
+      Drawing = new TDrawing(aBackColor, aForeColor);
+    }
+
+    //
+    //**********************************************************************************************
+    //
+
+    public Bitmap GetBitmap(Boolean aLegend)
+    {
+      return Drawing.GetBitmap(aLegend);
+    }
+
+    //
+    //**********************************************************************************************
+    //
+
+    public void DrawDerivedLegend(List<TDerivedClass> aClass)
+    {
+      Drawing.DrawDerivedLegend(aClass);
+    }
+
+    //
+    //**********************************************************************************************
+    //
+
+    public void DrawLegend(List<TMoistureClass> aClass)
+    {
+      Drawing.DrawLegend(aClass);
+    }
+    public void SetDrawingArea(Int32 aWidth, Int32 aHeight)
+    {
+      PlotWidth = aWidth;
+      PlotHeight = aHeight;
+      Drawing.SetDrawingArea(PlotWidth, PlotHeight);
+    }
+
+  }
+}
---
+++ Sources/TPoint.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDRFree
+{
+  class TPoint
+  {
+    public Double X;
+    public Double Y;
+    //
+    //**********************************************************************************************
+    //
+    // Class TPlot
+    //   Part of  : TPoint
+    //   Function : A simple point with x and y
+    //   Author   : Jan G. Wesseling
+    //   Date     : April 9th, 2013
+    //
+    //**********************************************************************************************
+    //
+    public TPoint(Double aX, Double aY)
+    {
+      X = aX;
+      Y = aY;
+    }
+  }
+}
